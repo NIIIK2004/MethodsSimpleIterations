4315599a6164f85ea70923215e4d2229a263d450
Nikita <exsione27@gmail.com>
2023-11-17 16:11:52 +0300

    update: Если пользователь не вводит количество итераций то программа сама считывает, и будет столько итераций пока результат не будет меньше эпсиланта (10 в минус 3 степени)

--- js/main.js
+++ js/main.js
@@ -11,17 +11,23 @@ function runIteration() {
     const b = [+document.getElementById('b0').value, + document.getElementById('b1').value, +document.getElementById('b2').value, +document.getElementById('b3').value];
     // Ввод начального приближения
     const x0 = [0, 0, 0, 0];
-    const iterations = +document.getElementById('iterations').value || 5;
-    const result = simpleIterationMethod(A, b, x0, iterations);
 
+    const iterations = +document.getElementById('iterations').value;
+
+    const epsilon = Math.pow(10, -3);
+
+    const result = simpleIterationMethod(A, b, x0, iterations);
 
 }
 
-function simpleIterationMethod(A, b, x0, iterations) {
+function simpleIterationMethod(A, b, x0, epsilon) {
     const n = A.length;
     let x = new Array(n);
+    let k = 0;
+    let error = Number.POSITIVE_INFINITY;
 
-    for (let k = 0; k < iterations; k++) {
+
+    do {
         for (let i = 0; i < n; i++) {
             let sum = 0.0;
             for (let j = 0; j < n; j++) {
@@ -32,17 +38,37 @@ function simpleIterationMethod(A, b, x0, iterations) {
             x[i] = (b[i] - sum) / A[i][i];
         }
 
-        x0 = [...x];
+        
 
         const outputElement = document.getElementById('output');
         const iterationText = `<p style="color:#8e8e8e; font-size: 29px;">Итерация</p> <p>(${k + 1}): {${x.join(', ')}}</p>`;
         const lineDivider = `<div class="line"></div>`;
 
         outputElement.innerHTML += `<div class="fade-in">${iterationText}${lineDivider}</div>`;
+
+        error = calculateError(x, x0);
+        x0 = [...x];
+        k++;
+
+    } while (error > epsilon)l
+}
+
+function calculateError(x, x0) {
+    let maxError = 0;
+
+    for (let i = 0; i < x.length; i++) {
+        const error = Math.abs(x[i] - x0[i]);
+        if (error > maxError) {
+            maxError = error;
+        }
     }
 
+    return maxError;
 }
+
 
 function clearOutput() {
     document.getElementById('output').innerHTML = '';
 }
+
+
